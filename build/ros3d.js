47e7f5706cd4aef2e29c231c3adfea79586ad5cc
Kai Koehn <kai.koehn@gmail.com>
2019-07-01 16:41:50 +0200

    Dispose function for points

--- build/ros3d.js
+++ build/ros3d.js
@@ -9463,7 +9463,7 @@ class Points extends THREE$1.Object3D {
 
   setup(frame, point_step, fields)
   {
-      if(this.sn===null){
+      if(this.sn===null && !this.destroyed){
           // turn fields to a map
           fields = fields || [];
           this.fields = {};
@@ -9531,6 +9531,14 @@ class Points extends THREE$1.Object3D {
       this.colors.needsUpdate = true;
       this.colors.updateRange.count = n * this.colors.itemSize;
     }
+  };
+
+  dispose(n)
+  {
+    this.destroyed = true;
+    if (this.positions) {this.positions.array = null;}
+    if (this.colors) {this.colors.array = null;}
+    this.rootObject.remove(this.sn);
   };
 }
 
@@ -9687,6 +9695,7 @@ class PointCloud2 extends THREE$1.Object3D {
     if(this.rosTopic){
       this.rosTopic.unsubscribe();
     }
+    this.points.dispose();
   };
 
   subscribe(){
@@ -9704,6 +9713,8 @@ class PointCloud2 extends THREE$1.Object3D {
   };
 
   processMessage(msg){
+    if (this.points.destroyed) {return}
+
     if(!this.points.setup(msg.header.frame_id, msg.point_step, msg.fields)) {
         return;
     }
